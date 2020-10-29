94bf3d2a8e159dc4d6ef6128f64ab30b6d4a61ec
Michael Maton <mjmaton6@gmail.com>
2020-10-29 14:54:07 -0500

    my card working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,20 @@
     https://api.github.com/users/<your name>
 */
 
+import axios from 'axios';
+// let me = axios.get('https://api.github.com/users/michael-maton');
+// let myCard = cardMaker(me);
+  // .this((res) => {
+  //   console.log(res);
+  // })
+axios
+  .get('https://api.github.com/users/michael-maton')
+  .then((res) => {
+    let myCard = cardMaker(res);
+    cards.appendChild(myCard);
+  })
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -16,6 +30,8 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -49,6 +65,94 @@ const followersArray = [];
       </div>
     </div>
 */
+
+const cards = document.querySelector('.cards');
+
+function cardMaker (dataObj){
+
+  let card = document.createElement('div');
+  card.className = 'card';
+  let imgSrc = document.createElement('img');
+  imgSrc.src = dataObj.data.avatar_url;
+  let cardInfo = document.createElement('div');
+  cardInfo.className = 'card-info';
+  let nameElement = document.createElement('h3');
+  nameElement.className = 'name';
+  nameElement.textContent = dataObj.data.name;
+  let username = document.createElement('p');
+  username.className = 'username';
+  username.textContent = dataObj.data.login;
+  let userLocation = document.createElement('p');
+  userLocation.textContent = `Location: ${dataObj.data.location}`;
+  let userProfile = document.createElement('p');
+  userProfile.textContent = 'Profile: ';
+  let address = document.createElement('a');
+  address.href = dataObj.data.url;
+  address.textContent = dataObj.data.url;
+  let userFollowers = document.createElement('p');
+  userFollowers.textContent = `Followers: ${dataObj.data.followers}`;
+  let userFollowing = document.createElement('p');
+  userFollowing.textContent = `Following: ${dataObj.data.following}`;
+  let userBio = document.createElement('p');
+  userBio.textContent = `Bio: ${dataObj.data.bio}`;
+
+  card.appendChild(imgSrc);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(nameElement);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userProfile);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFollowing);
+  cardInfo.appendChild(userBio);
+  userProfile.appendChild(address);
+  console.log(card);
+
+  return card;
+}
+
+
+
+
+// function cardMaker (dataObj){
+
+//   let card = document.createElement('div');
+//   card.className = 'card';
+//   let imgSrc = document.createElement('img');
+//   let cardInfo = document.createElement('div');
+//   cardInfo.className = 'card-info';
+//   let nameElement = document.createElement('h3');
+//   nameElement.className = 'name';
+//   let username = document.createElement('p');
+//   username.className = 'username';
+//   let userLocation = document.createElement('p');
+//   userLocation.textContent = `Location: `;
+//   let userProfile = document.createElement('p');
+//   userProfile.textContent = 'Profile: ';
+//   let address = document.createElement('a');
+//   let userFollowers = document.createElement('p');
+//   userFollowers.textContent = `Followers: `;
+//   let userFollowing = document.createElement('p');
+//   userFollowing.textContent = `Following: `;
+//   let userBio = document.createElement('p');
+//   userBio.textContent = `Bio: `;
+
+//   card.appendChild(imgSrc);
+//   card.appendChild(cardInfo);
+//   cardInfo.appendChild(nameElement);
+//   cardInfo.appendChild(username);
+//   cardInfo.appendChild(userLocation);
+//   cardInfo.appendChild(userProfile);
+//   cardInfo.appendChild(userFollowers);
+//   cardInfo.appendChild(userFollowing);
+//   cardInfo.appendChild(userBio);
+//   userProfile.appendChild(address);
+//   console.log(card);
+
+//   return card;
+// }
+
+
 
 /*
   List of LS Instructors Github username's:
